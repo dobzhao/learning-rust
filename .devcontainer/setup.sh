30240c2c90be82dc5d7aaadd8d039a5f1c8bf554
Dobby Zhao <dob@DobbydeMacBook-Pro.local>
2023-03-17 15:49:55 +0800

    ubuntu22 and extentions

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -14,11 +14,9 @@ apt-get install -y \
 
 ## Install rustup and common components
 curl https://sh.rustup.rs -sSf | sh -s -- -y 
-rustup install nightly
+source "$HOME/.cargo/env"
 rustup component add rustfmt
-rustup component add rustfmt --toolchain nightly
 rustup component add clippy 
-rustup component add clippy --toolchain nightly
 
 cargo install cargo-expand
 cargo install cargo-edit
